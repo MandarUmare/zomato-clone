4bf8a07f59dbca67ea722e3bbb53d23afd267798
MandarUmare <90469953+MandarUmare@users.noreply.github.com>
2023-03-13 16:15:22 +0530

    Updates

---
+++ precache-manifest.24773747ef1637970e986081672a0b24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad471a8e4353fd131ccf",
+    "url": "/zomato-clone/static/css/main.ffd8e66f.chunk.css"
+  },
+  {
+    "revision": "ad471a8e4353fd131ccf",
+    "url": "/zomato-clone/static/js/main.ad471a8e.chunk.js"
+  },
+  {
+    "revision": "ad5e43fd429b57f4e9c2",
+    "url": "/zomato-clone/static/css/1.7056d0b7.chunk.css"
+  },
+  {
+    "revision": "ad5e43fd429b57f4e9c2",
+    "url": "/zomato-clone/static/js/1.ad5e43fd.chunk.js"
+  },
+  {
+    "revision": "07df464a53c36d8a0e3e",
+    "url": "/zomato-clone/static/js/runtime~main.07df464a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/zomato-clone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/zomato-clone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/zomato-clone/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/zomato-clone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "106c0c06a196da0f4ac2a273d5aa0194",
+    "url": "/zomato-clone/index.html"
+  }
+];
